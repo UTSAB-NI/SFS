442b585991eaf795ca0693e93358b33b0e76bc85
utsab singh <utsabsingh5@gmail.com>
2024-09-10 15:26:36 +0545

    fix appointment issue

--- SFS/Areas/Admin/Controllers/AppointmentController.cs
+++ SFS/Areas/Admin/Controllers/AppointmentController.cs
@@ -1,46 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SFS.Data;
 using SFS.Models;
-using SFS.Utilities;
 
-namespace SFS.Areas.Client.Controllers
+namespace SFS.Areas.Admin.Controllers
 {
-    [Area("Client")]
-    [Authorize]
+    [Area("Admin")]
     public class AppointmentController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
 
-        public AppointmentController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        public AppointmentController(ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
-        // GET: Client/Appointment
+        // GET: Admin/Appointment
         public async Task<IActionResult> Index()
         {
-            var userId = _userManager.GetUserId(User);
-
-            var applicationDbContext = _context.Appointments
-                .Where(a => a.ApplicationUserId == userId)
-                .Include(a => a.Schedule)
-                .ThenInclude(a=>a.Class)
-                .Include(a => a.User);
+            var applicationDbContext = _context.Appointments.Include(a => a.Schedule).ThenInclude(a => a.Class).Include(a => a.User);
             return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Client/Appointment/Details/5
+        // GET: Admin/Appointment/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -61,21 +48,17 @@ namespace SFS.Areas.Client.Controllers
             return View(appointment);
         }
 
-        // GET: Client/Appointment/Create
-        public IActionResult Create(int? scheduleId)
+        // GET: Admin/Appointment/Create
+        public IActionResult Create()
         {
-            ViewData["ScheduleId"] = scheduleId;
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            ViewData["ApplicationUserId"] = userId;
-            var schedule= _context.Schedules.Include("Class").FirstOrDefault(s => s.ScheduleId == scheduleId);
-            ViewData["ClassName"] = schedule.Class.Name;
-            ViewData["StartTime"] = schedule.StartTime;
-            ViewData["EndTime"] = schedule.EndTime;
-            //ViewData["ApplicationUserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            ViewData["ScheduleId"] = new SelectList(_context.Schedules, "ScheduleId", "ScheduleId");
+            ViewData["ApplicationUserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
             return View();
         }
 
-       
+        // POST: Admin/Appointment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("AppointmentId,ScheduleId,ApplicationUserId,AppointmentDate")] Appointment appointment)
@@ -91,7 +74,7 @@ namespace SFS.Areas.Client.Controllers
             return View(appointment);
         }
 
-        // GET: Client/Appointment/Edit/5
+        // GET: Admin/Appointment/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -109,7 +92,7 @@ namespace SFS.Areas.Client.Controllers
             return View(appointment);
         }
 
-        // POST: Client/Appointment/Edit/5
+        // POST: Admin/Appointment/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -146,7 +129,7 @@ namespace SFS.Areas.Client.Controllers
             return View(appointment);
         }
 
-        // GET: Client/Appointment/Delete/5
+        // GET: Admin/Appointment/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -166,7 +149,7 @@ namespace SFS.Areas.Client.Controllers
             return View(appointment);
         }
 
-        // POST: Client/Appointment/Delete/5
+        // POST: Admin/Appointment/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
